7a9095db2ae226bfc8bce41621c827020a33205d
GdPaul1234 <paul.godin1234@outlook.fr>
2021-10-06 16:00:18 +0200

    add client and commande views

---
+++ Projet Pizzeria/View/ClientView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Projet_Pizzeria.View
+{
+    /// <summary>
+    /// Logique d'interaction pour ClientView.xaml
+    /// </summary>
+    public partial class ClientView : UserControl
+    {
+        public ClientView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Projet Pizzeria/View/CommandeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Projet_Pizzeria.View
+{
+    /// <summary>
+    /// Logique d'interaction pour CommandeView.xaml
+    /// </summary>
+    public partial class CommandeView : UserControl
+    {
+        public CommandeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
